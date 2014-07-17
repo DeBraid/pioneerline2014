62b1619c5bace27c7636d7fe85d015940caad6a8
DeBraid <de.braid@gmail.com>
2014-07-17 16:55:01 -0400

    added charges to all product templates

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -70,6 +70,114 @@ Router.map(function() {
     path: '/'
   });
 
+  this.route('latexonecolround', {
+    path: '/latexonecolround',
+    template: 'latexonecolround',
+    yieldTemplates: {
+      'charges': {to: 'charges'}
+    }, 
+    waitOn: function () {
+      return [Meteor.subscribe('latexonecolround'),
+              Meteor.subscribe('charges')];
+    },
+    data: function () {
+      return {
+        latexonecolround: Latexonecolround.find(),
+        charges: Charges.find()
+      }
+    }
+  });
+
+  this.route('roundballoons', {
+    path: '/roundballoons',
+    template: 'roundballoons',
+    yieldTemplates: {
+      'charges': {to: 'charges'}
+    }, 
+    waitOn: function () {
+      return [Meteor.subscribe('roundballoons'),
+              Meteor.subscribe('charges')];
+    },
+    data: function () {
+      return {
+        roundballoons: Roundballoons.find(),
+        charges: Charges.find()
+      }
+    }
+  });
+
+  this.route('heartballoons', {
+    path: '/heartballoons',
+    template: 'heartballoons',
+    yieldTemplates: {
+      'charges': {to: 'charges'}
+    }, 
+    waitOn: function () {
+      return [Meteor.subscribe('heartballoons'),
+              Meteor.subscribe('charges')];
+    },
+    data: function () {
+      return {
+        heartballoons: Heartballoons.find(),
+        charges: Charges.find()
+      }
+    }
+  });
+
+  this.route('quicklinkround', {
+    path: '/quicklinkround',
+    template: 'quicklinkround',
+    yieldTemplates: {
+      'charges': {to: 'charges'}
+    }, 
+    waitOn: function () {
+      return [Meteor.subscribe('quicklinkround'),
+              Meteor.subscribe('charges')];
+    },
+    data: function () {
+      return {
+        quicklinkround: Quicklinkround.find(),
+        charges: Charges.find()
+      }
+    }
+  });
+
+  this.route('threecolourplus', {
+    path: '/threecolourplus',
+    template: 'threecolourplus',
+    yieldTemplates: {
+      'charges': {to: 'charges'}
+    }, 
+    waitOn: function () {
+      return [Meteor.subscribe('threecolourplus'),
+              Meteor.subscribe('charges')];
+    },
+    data: function () {
+      return {
+        threecolourplus: Threecolourplus.find(),
+        charges: Charges.find()
+      }
+    }
+  });
+
+  this.route('twocolourballoons', {
+    path: '/twocolourballoons',
+    template: 'twocolourballoons',
+    yieldTemplates: {
+      'charges': {to: 'charges'}
+    }, 
+    waitOn: function () {
+      return [Meteor.subscribe('twocolourballoons'),
+              Meteor.subscribe('charges')];
+    },
+    data: function () {
+      return {
+        twocolourballoons: Twocolourballoons.find(),
+        charges: Charges.find()
+      }
+    }
+  });
+
   this.route('adwave', {
     path: '/adwave',
     template: 'adwave',
@@ -88,97 +196,20 @@ Router.map(function() {
     }
   });
 
-  this.route('latexonecolround', {
-    path: '/latexonecolround',
-    template: 'latexonecolround',
-    waitOn: function () {
-      return Meteor.subscribe('latexonecolround');
-    },
-    data: function () {
-      return {
-        latexonecolround: Latexonecolround.find()
-      }
-    }
-  });
-
-  this.route('roundballoons', {
-    path: '/roundballoons',
-    template: 'roundballoons',
-    waitOn: function () {
-      return Meteor.subscribe('roundballoons');
-    },
-    data: function () {
-      return {
-        roundballoons: Roundballoons.find()
-      }
-    }
-  });
-
-  this.route('heartballoons', {
-    path: '/heartballoons',
-    template: 'heartballoons',
-    waitOn: function () {
-      return Meteor.subscribe('heartballoons');
-    },
-    data: function () {
-      return {
-        heartballoons: Heartballoons.find()
-      }
-    }
-  });
-
-
-  this.route('quicklinkround', {
-    path: '/quicklinkround',
-    template: 'quicklinkround',
-    waitOn: function () {
-      return Meteor.subscribe('quicklinkround');
-    },
-    data: function () {
-      return {
-        quicklinkround: Quicklinkround.find()
-      }
-    }
-  });
-
-
-  this.route('threecolourplus', {
-    path: '/threecolourplus',
-    template: 'threecolourplus',
-    waitOn: function () {
-      return Meteor.subscribe('threecolourplus');
-    },
-    data: function () {
-      return {
-        threecolourplus: Threecolourplus.find()
-      }
-    }
-  });
-
-   this.route('twocolourballoons', {
-    path: '/twocolourballoons',
-    template: 'twocolourballoons',
-    waitOn: function () {
-      return Meteor.subscribe('twocolourballoons');
-    },
-    data: function () {
-      return {
-        twocolourballoons: Twocolourballoons.find()
-      }
-    }
-  });
-  
-
-
   this.route('adwrap', {
     path: '/adwrap',
     template: 'adwrap',
+    yieldTemplates: {
+      'charges': {to: 'charges'}
+    }, 
     waitOn: function () {
-      return Meteor.subscribe('adwrap');
+      return [Meteor.subscribe('adwrap'),
+              Meteor.subscribe('charges')];
     },
     data: function () {
       return {
-        adwrap: Adwrap.find()
+        adwrap: Adwrap.find(),
+        charges: Charges.find()
       }
     }
   });
@@ -198,9 +229,6 @@ Router.map(function() {
 
   this.route('content');
   this.route('contact');
-
-
-
   // Users
 
   this.route('login');
