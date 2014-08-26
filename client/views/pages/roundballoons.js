a5470836508d6809434836ea1794c4ca1b0d6739
DeBraid <de.braid@gmail.com>
2014-08-26 10:53:50 -0400

    fixed round balloons template

--- client/views/pages/roundballoons.js
+++ client/views/pages/roundballoons.js
@@ -2,44 +2,40 @@ Template.roundballoons.settings = function () {
     return {
         showFilter: true,
         fields: [{
-                        key: 'Product Description',
-                        label: 'Product',
-                        sort: 'descending',
-                        fn: function (name, object) {
-                            var image = object.PSD.slice(0,-4);
-                            var twofifty = '250';
-                            
-                            if ( name.indexOf("Jewel") > -1 ) {
-
-                                var html = '<h3 class="productname">' + name + '</h3>' + 
+                    key: 'Product Description',
+                    label: 'Product',
+                    sort: 'descending',
+                    fn: function (name, object) {
+                        var image = object.PSD.slice(0,-4);
+                        
+                            var html = '<h3 class="productname">' + name + '</h3>' + 
                                 '<div class="thumbnail tableimages">' + 
-                                     '<h1>' + object[250] + '</h1>' + 
+                                    '<img src="' + image + '.jpg' + '">' + 
                                 '</div>';
-                                return new Spacebars.SafeString(html);
-                            }     
-                        }
-                    }, {
-                        key: '250',
-                        label: '250'
-                    }, {
-                        key: '500',
-                        label: '500'
-                    }, {
-                        key: '1000',
-                        label: '1000'
-                    }, {
-                        key: '2500',
-                        label: '2500'
-                    }, {
-                        key: '5000',
-                        label: '5000'
-                    }, {
-                        key: '10000',
-                        label: '10000'
-                    }, {
-                        key: '25000',
-                        label: '25000'
-                    }
-                ]
+                            return new Spacebars.SafeString(html);
+                    }     
+                }, {
+                    key: '250',
+                    label: '250'
+                }, {
+                    key: '500',
+                    label: '500'
+                }, {
+                    key: '1000',
+                    label: '1000'
+                }, {
+                    key: '2500',
+                    label: '2500'
+                }, {
+                    key: '5000',
+                    label: '5000'
+                }, {
+                    key: '10000',
+                    label: '10000'
+                }, {
+                    key: '25000',
+                    label: '25000'
+                }
+            ]
     };
 };
