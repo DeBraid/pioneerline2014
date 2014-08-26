590f0b495dc36e7a4a5e2827016a9929debe6635
DeBraid <de.braid@gmail.com>
2014-08-26 18:41:55 -0400

    popup banners added

---
+++ client/views/pages/popupbanners.js
@@ -0,0 +1,39 @@
+Template.popupbanners.settings = function () {
+    return {
+        showFilter: true,
+        fields: [{
+                  key: 'Product Description',
+                  label: 'Product',
+                  fn: function (name, object) {
+
+                      var image = object.PSD.slice(0,-4);
+                      
+                      var html = '<h3 class="productname">' + name + '</h3>' + 
+                          '<div class="thumbnail tableimages">' + 
+                              '<img src="' + image + '.jpg' + '">' + 
+                          '</div>';
+
+                      return new Spacebars.SafeString(html);
+                    }
+                }, {
+                    key: 'ItemNum', 
+                    label: 'Item #'
+                }, {
+                    key: '1',
+                    label: '1'
+                }, {
+                    key: '2 to 5',
+                    label: '2 to 5'
+                }, {
+                    key: '6 to 11',
+                    label: '6 to 11'
+                }, {
+                    key: '12 to 24',
+                    label: '12 to 24' 
+                }, {
+                    key: '25 to 49',
+                    label: '25 to 49' 
+                }
+            ]
+    };
+};
