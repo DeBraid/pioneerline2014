7d5c6b6e900bde6a607de511b4a0d57db2c30edf
DeBraid <de.braid@gmail.com>
2014-08-26 23:43:08 -0400

    added underscore helper package for templates, hacking tg code to extract unique keywords in title

--- client/views/common/filterbuttons.js
+++ client/views/common/filterbuttons.js
@@ -13,3 +13,31 @@ Template.filterbuttons.events({
 
     }
 });
+
+Template.filterbuttons.helpers({
+  productTypes: function () {
+    $('.productname').map(function () {
+      var words = [];
+      words.push($(this).text().split(' '))
+      data.push(_.flatten(words), false)
+    });
+      function eliminateDuplicates(arr) {
+          var i,
+            len=arr.length,
+            out=[],
+            obj={};
+
+           for (i=0;i<len;i++) {
+           obj[arr[i]]=0;
+           }
+           for (i in obj) {
+           out.push(i);
+           }
+           return out;
+      }
+
+    
+      eliminateDuplicates(_.flatten(data))
+      console.log(data);
+  }
+});
