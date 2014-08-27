02552e0f5134a1c2beba9eb4e5cfaee649d0c3ce
DeBraid <de.braid@gmail.com>
2014-08-27 14:55:05 -0400

    new batch of images, cleaned up adrite

--- client/views/pages/adriteeconomyline.js
+++ client/views/pages/adriteeconomyline.js
@@ -6,8 +6,8 @@ Template.adriteeconomyline.settings = function () {
                   label: 'Product',
                   fn: function (name, object) {
 
-                      // var image = object.PSD.slice(0,-4);
-                      var image = 'placeholder';
+                      var image = object.PSD.slice(0,-4);
+                      
                       var html = '<h3 class="productname">' + name + '</h3>' + 
                             '<div class="thumbnail tableimages">' + 
                                 '<img src="' + image + '.jpg' + '">' + 
