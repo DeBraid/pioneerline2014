87a07e77c9b956846ad16344779a7bd2dd689b94
DeBraid <de.braid@gmail.com>
2014-08-27 15:27:10 -0400

    more pics, cleaned up cloudbuster and adrite

--- client/views/pages/cloudbuster.js
+++ client/views/pages/cloudbuster.js
@@ -1,13 +1,15 @@
 Template.cloudbuster.settings = function () {
     return {
         showFilter: true,
+        rowsPerPage: 5,
+        showNavigation: 'auto',
         fields: [{
                   key: 'Product Description',
                   label: 'Product',
                   fn: function (name, object) {
 
-                      // var image = object.PSD.slice(0,-4);
-                      var image = 'placeholder';
+                      var image = object.PSD.slice(0,-4);
+                      
                       var html = '<h3 class="productname">' + name + '</h3>' + 
                             '<div class="thumbnail tableimages">' + 
                                 '<img src="' + image + '.jpg' + '">' + 
@@ -18,9 +20,6 @@ Template.cloudbuster.settings = function () {
                 }, {
                     key: 'Colours',
                     label: 'Colours'
-                }, {
-                    key: 'Printing',
-                    label: 'Printing'
                 }, {
                     key: '1',
                     label: '1'
